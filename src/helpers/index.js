cd24966347b951e2d7c2caff8216c786a00d4089
ilya_shyian <ilya.shyian@gmail.com>
2022-06-08 23:43:52 +0300

    update backendURL

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,5 +3,5 @@ import { mock } from './mock';
 import { delay } from './delay';
 import { statusNumber, statusOptions } from './orderStatus';
 
-export const backendURL = 'https://diploma-back-django.herokuapp.com/';
+export const backendURL = 'https://diploma-back-django.herokuapp.com';
 export { jwtDecode, mock, delay, statusNumber, statusOptions };
