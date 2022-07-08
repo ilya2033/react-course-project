693cbc833b9e6facf8596d648354b1443ba7d075
ilya_shyian <ilya.shyian@gmail.com>
2022-07-08 15:46:01 +0300

    remove mediaURl from src path

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,5 +6,5 @@ import { statusNumber, statusOptions } from "./orderStatus";
 
 export const backendURL = "http://188.72.209.29/api";
 export const gql = getGQL(backendURL + "/graphql/");
-export const mediaURL = "/media/";
+export const mediaURL = "";
 export { jwtDecode, delay, statusNumber, statusOptions };
